a146076c9099e09eef159f6e36ba7216c11d1207
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-09 16:59:06 +0530

    modified:   app.js 	modified:   content/first.txt 	new file:   content/second.txt

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const {readFileSync, writeFileSync} = require('fs')
 
+const first = readFileSync('./content/first.txt','utf-8')
+const second = readFileSync('./content/second.txt','utf-8')
 
+console.log(first, second)
