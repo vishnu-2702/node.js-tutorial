f942b97d19fe837811bbae78ec127a4d4e116f31
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-09 17:05:07 +0530

    modified:   app.js 	new file:   content/result-sync.txt

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const {readFileSync, writeFileSync} = require('fs')
 
+// reading content from file 
 const first = readFileSync('./content/first.txt','utf-8')
 const second = readFileSync('./content/second.txt','utf-8')
 
 console.log(first, second)
+
+//creating a file and writing its content
+
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result : ${first}, ${second}`,
+    {flag: 'a'}
+)
