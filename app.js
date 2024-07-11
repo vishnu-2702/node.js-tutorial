519b5ee3826022d9fc4846f3939d2f5e649393c2
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-11 16:59:54 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const server = http.createServer((req, res) => {
     if(req.url === '/'){
         res.end('Welcome to our home page')
     }
+    if(req.url === 'about'){
+        res.end('Here is our short about')
+    }
 })
 
 server.listen(5000)
