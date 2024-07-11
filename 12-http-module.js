1f1c9df1302104298e4b427635b948d426b79f62
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-11 17:07:22 +0530

    renamed:    app.js -> 12-http-module.js

--- 12-http-module.js
+++ 12-http-module.js
@@ -4,9 +4,14 @@ const server = http.createServer((req, res) => {
     if(req.url === '/'){
         res.end('Welcome to our home page')
     }
-    if(req.url === 'about'){
+    if(req.url === '/about'){
         res.end('Here is our short about')
     }
+    res.end(`
+        <h1>Error 404</h1>
+        <p>We can't seem to find the page you are looking for</p>
+        <a href="/">back home</a>
+    `)
 })
 
 server.listen(5000)
