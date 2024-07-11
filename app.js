f04c63e13adae44cee65b8fb0af6d55593e98a60
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-11 17:32:35 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -7,3 +7,8 @@
 
 // global dependency - use it in any project
 // npm install -g <packagName>
+
+// package.json - manifest file( stores important informaiton about the project/packages)
+// manual approach ( create a package.json in the roor, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything is default)
