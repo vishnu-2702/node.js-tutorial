69e663e556a82ffe6fafc77d8f9c7b445a47156a
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-11 17:24:59 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// nom - global
+// nom - global command, come with node
+// npm --versions 
+// npm --v
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packagName>
