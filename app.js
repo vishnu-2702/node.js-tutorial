727e78a76828bb5bcb54b0ffd9027d5d23b2f407
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-13 17:41:45 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const { readFile } = require('fs')
+const {readFile} = require('fs')
+
+
+console.log('started a first task')
+// Check File Path
+
+readFile('./content/first.txt', 'utf-8', (err, result) =>{
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+})
+
+console.log('starting next task')
