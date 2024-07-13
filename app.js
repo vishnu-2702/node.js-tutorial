3828395af3e7748b241a2a758e46a07cae36619e
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-13 17:54:51 +0530

    new file:   app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+    console.log('hello world')
+}, 2000)
+console.log('I will run first')
+// the process will stay alive until the processed is killed press ctrl+C to kill the process
