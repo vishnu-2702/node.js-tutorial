6754d5db0afa5291008e9034f03d1bee06c2e067
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-14 12:35:53 +0530

    renamed:    app.js -> 14-event-emitter.js

--- 14-event-emitter.js
+++ 14-event-emitter.js
@@ -2,11 +2,11 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter()
 
-customEmitter.on('response',()=>{
-    console.log('data received')
+customEmitter.on('response',(name, id)=>{
+    console.log(`data received user: ${name} with id: ${id}`)
 })
 customEmitter.on('response',()=>{
     console.log('some other logic')
 })
 
-customEmitter.emit('response')
+customEmitter.emit('response', 'john', 34)
