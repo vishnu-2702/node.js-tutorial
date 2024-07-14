f6e1319668fc2c7bf6715a03a36eb67c40d7997a
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-14 12:31:25 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const customEmitter = new EventEmitter()
 customEmitter.on('response',()=>{
     console.log('data received')
 })
+customEmitter.on('response',()=>{
+    console.log('some other logic')
+})
 
 customEmitter.emit('response')
