78ec61edaaff7250350a9137205075dac0d1464c
vishnu-2702 <vishnu.v2704@gmail.com>
2024-07-14 12:20:08 +0530

    modified:   app.js 	new file:   content/result-mind-grenade.txt 	modified:   app.js

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+const start = async () => {
+  try {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile(
+      './content/result-mind-grenade.txt',
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: 'a' }
+    )
+    console.log(first, second)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
+/*
 const {readFile} = require('fs')
 
 
@@ -17,3 +39,4 @@ const getText = (path) =>{
 getText('./content/first.txt')
 .then(result => console.log(result))
 .catch((err) => console.log(err))
+*/
